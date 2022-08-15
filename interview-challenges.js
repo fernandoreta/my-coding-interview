5a239ee8eeb2c39a355440cc48b72ce46a02c672
Fernando Wertt <fer19_reta@hotmail.com>
2022-08-14 21:55:57 -0500

    1.1 1.2 problems

---
+++ interview-challenges.js
@@ -0,0 +1,5 @@
+// 1.1 Implement an algorithm to determine if a string has all unique chars.
+// What if you cannot use additional data structures?
+
+// 1.2 CHeck Permutation. Given 2 strings, write a method to decide if one is
+// a permutation of the other 'abcd' and 'dabc' -> true
